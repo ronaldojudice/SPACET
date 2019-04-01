15328449658dfb7f2617fffd58b83b05f0d6560c
ronaldojudice <49134678+ronaldojudice@users.noreply.github.com>
2019-03-31 22:42:48 -0300

    Add files via upload
    SPACET

---
+++ SPACET/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//========================================================
+//index/índice
+//========================================================
+
+
+include_once('_cabecalho.php');
+
+echo '<p>Teste de Font Awesome <span class="fa fa-times"></span></p>';
+
+include_once('_rodape.php');
+
+?>
